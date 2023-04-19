6a0da39ec70b1608c83c7fb0fcff2ca663074134
Jens <sotikov1@gmail.com>
2023-04-19 16:46:36 +0300

    viimeiset muutokset

--- models/userr_model.js
+++ models/userr_model.js
@@ -1,17 +1,31 @@
 const db = require('../database');
+const saltRounds=10;
+const bcrypt = require('bcryptjs');
 
 const userr = {
 
     getUserbyid: function(id, callback) {
-        return db.query('select * from userr where userid= $1',[id], callback);
+        return db.query('select * from userr where userid= $1;',[id], callback);
     },
 
     getUserAll: function(callback) {
         return db.query('select * from userr', callback);
     },
 
-    add: function(newUser, callback){
-        return db.query('insert into userr values (?,?,?,?,?,?)');
+    add: function(params, callback) {
+        return db.query('insert into userr (userid, email, password, fname, lname, phonenumber) values ($1, $2, $3, $4, $5, $6);',
+        [params.userid, params.email, params.password, params.fname, params.lname, params.phonenumber], callback);
+    },
+    
+    delete: function(id, callback) {
+        return db.query('delete from userr where userid= $1;', [id], callback);
+    },
+    
+    update: function(id, user, callback) {
+        bcrypt.hash(user.password, saltRounds, function(err, hash) {
+          return db.query('update userr set email=$1, password=$2, fname=$3, lname=$4, phonenumber=$5 where userid=$1;',
+          [user.email, hash, user.fname, user.lname, user.phonenumber, id], callback);
+        });
     }
     
 }
--- routes/userr.js
+++ routes/userr.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userr = require('../models/userr_model');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/:id',
  function(request, response) {
@@ -16,27 +17,61 @@ router.get('/:id',
   }
 });
 
-router.get('',function (response) {
+router.get('/', function (req, res) {
   userr.getUserAll(function(err, dbResult) {
     if (err) {
-      response.json(err);
+      console.log(err);
     } else {
       let data = dbResult;
-      response.json(data.rows);
+      try{
+        res.json(data.rows)
+      } catch(err){
+        res.send("nothing found")
+      }
     }
   });   
 });
 
-router.post('/', 
-function(request, response) {
-  userr.add(request.body, function(err, count) {
+router.post('/', function(req, response) {
+
+  let params = {
+    userid: req.body.userid,
+    fname: req.body.fname,
+    lname: req.body.lname,
+    email: req.body.email,
+    phonenumber: req.body.phonenumber,
+    password: req.body.password
+}
+
+  userr.add(params, function(err, dbResult) {
     if (err) {
       response.json(err);
     } else {
-      response.json(request.body); 
+      response.json(dbResult);
     }
   });
 });
 
+router.delete('/:id',
+function(request, response) {
+  userr.delete(request.params.id, function(err, count) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(count);
+    }
+  });
+});
+
+router.put('/:id',
+function(request, response) {
+  userr.update(request.params.id, request.body, function(err, dbResult) {
+    if (err) {
+      response.json(err);
+    } else {
+      response.json(dbResult);
+    }
+  });
+});
 
 module.exports = router;
